90cc01b1b859b29399d701ced824a8fa1d81dbef
filipedower <filipesilva504@gmail.com>
2024-03-03 00:24:47 -0300

    implementado redux slice de categories

---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Category {
+    id: string;
+    name: string;
+    is_active: boolean;
+    created_at: string;
+    updated_at: string;
+    deleted_at: null | string;
+    description: null | string;
+}
+
+const category: Category = {
+    id: "550e8400-e29b-41d4-a716-446655440000",
+    name: "Olive",
+    description: "Este é um exemplo de objeto JSON.",
+    is_active: true,
+    deleted_at: "2024-03-03T12:00:00Z",
+    created_at: "2024-03-03T10:00:00Z",
+    updated_at: "2024-03-03T11:00:00Z"
+};
+
+export const initialState = [
+    category,
+    {...category, id: "650e8400-e29b-41d4-a716-446655440000", name: "Peach"},
+    {...category, id: "750e8400-e29b-41d4-a716-446655440000", name: "Apple"},
+    {...category, id: "850e8400-e29b-41d4-a716-446655440000", name: "Banana"},
+]
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState: initialState,
+    reducers: {
+        createCategory(state, action) {},
+        updateCategory(state, action) {},
+        deleteCategory(state, action) {},
+    }
+});
+
+export default categoriesSlice.reducer;
