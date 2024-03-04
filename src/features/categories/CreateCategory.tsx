a0a49af1f44750569659f2ae71b072c50e6121d4
filipedower <filipesilva504@gmail.com>
2024-03-04 18:07:25 -0300

    criando create form e refatorando o componente form para ser reutilizado

--- src/features/categories/CreateCategory.tsx
+++ src/features/categories/CreateCategory.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, FormControl, FormControlLabel, FormGroup, Grid, Paper, Switch, TextField, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { Category } from "./categorySlice";
+import { CategoryForm } from "./components/CategoryForm";
 
 export const CreateCategory = () => {
   const [isDisabled, setIsDisabled] = useState(false);
@@ -28,71 +28,14 @@ export const CreateCategory = () => {
           </Box>
         </Box>
 
-        <Box p={2}>
-          <form>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField 
-                    required
-                    name="name"
-                    label="Name"
-                    value={category.name}
-                    disabled={isDisabled}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField 
-                    required
-                    name="description"
-                    label="Description"
-                    value={category.description}
-                    disabled={isDisabled}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormGroup>
-                  <FormControlLabel 
-                    control={
-                      <Switch 
-                        name="is_active"
-                        color="secondary"
-                        onChange={handleToggle}
-                        checked={category.is_active}
-                        inputProps={{ "aria-label": "controlled" }}
-                      />
-                    }
-                    label="Active"
-                  />
-                </FormGroup>
-              </Grid>
-
-              <Grid item xs={12}>
-                <Box display="flex" gap={2}>
-                  <Button variant="contained" component={Link} to="/categories">
-                    Back
-                  </Button>
-
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="secondary"
-                    disabled={isDisabled}
-                  >
-                    Save
-                  </Button>
-                </Box>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
+        <CategoryForm
+          category={category}
+          isDisabled={isDisabled}
+          isLoading={false}
+          onSubmit={() => {}}
+          handleChange={handleChange}
+          handleToggle={handleToggle}
+        />
       </Paper>
     </Box>
   )
