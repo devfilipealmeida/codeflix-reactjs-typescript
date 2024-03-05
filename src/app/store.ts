789dcbba7e3c6c50544bb0d53c24aa1a818e9938
filipedower <filipesilva504@gmail.com>
2024-03-05 18:25:18 -0300

    removid counter padrao do rtk

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import categoriesReducer from '../features/categories/categorySlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     categories: categoriesReducer,
   },
 });
