433414cae37cd762803323911409e18133f9b655
filipedower <filipesilva504@gmail.com>
2024-03-05 18:55:56 -0300

    implementado apiSlice e category reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import categoriesReducer from '../features/categories/categorySlice';
+import { apiSlice } from '../features/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     categories: categoriesReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
 });
 
---
+++ src/features/api/apiSlice.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const baseUrl = 'https://localhost:8000/api';
+
+export const apiSlice = createApi({
+    reducerPath: "api",
+    tagTypes: ["Categories"],
+    endpoints: (builder) => ({}),
+    baseQuery: fetchBaseQuery({ baseUrl }),
+});
